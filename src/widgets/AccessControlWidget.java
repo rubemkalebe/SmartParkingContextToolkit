4a02742c0bd753b1a943e63968639790e1db697d
Stefano Loss <Stefano Loss@10.7.173.141>
2016-08-31 11:05:09 -0300

    AccessControl

---
+++ src/widgets/AccessControlWidget.java
@@ -0,0 +1,32 @@
+package widgets;
+
+import context.arch.storage.Attribute;
+import context.arch.storage.AttributeNameValue;
+import context.arch.widget.Widget;
+import enums.TypeEnum;
+
+public class AccessControlWidget extends Widget {
+	
+	public static final String CLASSNAME = AccessControlWidget.class.getName();
+	public static final String ID = "identifier";
+	public static final String AUTENTICATION = "autentication";	
+	public static final String VACANCY = "spot available";
+	
+	private String gateID;
+
+	public AccessControlWidget(String id) {
+		super(CLASSNAME,CLASSNAME);
+		this.gateID = id;
+		super.start(true);
+	}
+	
+	@Override
+	protected void init() {
+		
+		addAttribute(Attribute.instance(AUTENTICATION, String.class));
+		addAttribute(Attribute.instance(VACANCY, Boolean.class));
+		
+		// constant attributes
+		addAttribute(AttributeNameValue.instance(ID, gateID), true);
+	}
+}
--- src/widgets/DriverWidget.java
+++ src/widgets/DriverWidget.java
@@ -9,6 +9,7 @@ public class DriverWidget extends Widget {
 	
 	public static final String CLASSNAME = DriverWidget.class.getName();
 	public static final String ID = "identifier"; //leia-se CPF
+	public static final String PERMISSION = "permission"; 
 	public static final String CREDIT_CARD = "credit_card"; 
 	public static final String CURRENT_SPOT = "current_spot"; 
 	public static final String TYPE = "type";
@@ -24,6 +25,7 @@ public class DriverWidget extends Widget {
 	@Override
 	protected void init() {
 		
+		addAttribute(Attribute.instance(PERMISSION, Boolean.class));
 		addAttribute(Attribute.instance(CREDIT_CARD, String.class));
 		addAttribute(Attribute.instance(CURRENT_SPOT, String.class));
 		addAttribute(Attribute.instance(TYPE, TypeEnum.class));	
