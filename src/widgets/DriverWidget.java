8f7fdb9bbdfe33def9789333edac8ae54a85b4ce
Stefano Loss <Stefano Loss@10.7.173.141>
2016-08-31 09:44:42 -0300

    Criação das Widgets

---
+++ src/widgets/DriverWidget.java
@@ -0,0 +1,14 @@
+package widgets;
+
+import context.arch.widget.Widget;
+
+public class DriverWidget extends Widget {
+
+	public DriverWidget(int port, String id, String widgetClassName) {
+		super(port, id, widgetClassName);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
---
+++ src/widgets/SpotWidget.java
@@ -0,0 +1,14 @@
+package widgets;
+
+import context.arch.widget.Widget;
+
+public class SpotWidget extends Widget {
+
+	public SpotWidget(String id, String widgetClassName) {
+		super(id, widgetClassName);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
