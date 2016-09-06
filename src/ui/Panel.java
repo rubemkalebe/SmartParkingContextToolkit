1e1cd2531411bc0a2237d9510045efe883fcc1b5
Stefano Loss <Stefano Loss@10.51.65.44>
2016-09-06 14:18:28 -0300

    Lógica da Inerface

--- src/ui/Panel.java
+++ src/ui/Panel.java
@@ -2,11 +2,17 @@ package ui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.peer.MenuPeer;
+import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel {
+public class Panel extends JPanel {	
 	
 	public void paintComponent( Graphics g ){
 	    super.paintComponent( g );
@@ -32,22 +38,34 @@ public class Panel extends JPanel {
             g.fillRect( 92+(i*80), 50 , 77, 100);
             g.fillRect( 92+(i*80), 155 , 77, 100);
         }
-        
-        g.setColor( Color.RED );
-        g.fillRect( 92, 50 , 77, 100);
-	}		
+	}
 	
-	
-	
+	public void occupingSpot( int spot, Graphics g ){		
+	    if (spot < 7){
+	    	g.setColor( Color.RED );
+	    	g.fillRect( 92+((spot-1)*80), 50 , 77, 100);	    	
+	    }	    
+	    else{
+	    	g.setColor( Color.RED );
+	    	g.fillRect( 92+((spot-7)*80), 155 , 77, 100);	    	
+	    }
+	}
 	
 	public static void main(String[] args) {
 		JFrame janela = new JFrame("SmartParking");
-		Panel meuPainel = new Panel();
+		Panel meuPainel = new Panel();		
 		
 		janela.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		janela.add(meuPainel);
 		janela.setSize(600,600);
 		janela.setVisible(true);
+		
+		//lÊ vaga ocupada
+		Scanner scanner = new Scanner(System.in); 		
+		int i = scanner.nextInt();
+		
+		meuPainel.occupingSpot(i, janela.getGraphics());
+				
 	    }
 	
 }
